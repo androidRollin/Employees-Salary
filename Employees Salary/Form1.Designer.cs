3688c9ffe01f136c4c8d0e2d95fbae403cda984d
Roline Aguilar <69499178+androidRollin@users.noreply.github.com>
2021-06-12 20:28:15 +0800

    Add project files.

---
+++ Employees Salary/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿
+namespace Employees_Salary
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmpName = new System.Windows.Forms.Label();
+            this.lblHrsWorked = new System.Windows.Forms.Label();
+            this.lblEmpRank = new System.Windows.Forms.Label();
+            this.lblGrossSalary = new System.Windows.Forms.Label();
+            this.lblDeductions = new System.Windows.Forms.Label();
+            this.cbGSIS = new System.Windows.Forms.CheckBox();
+            this.cbWithholdingTax = new System.Windows.Forms.CheckBox();
+            this.cbPhilHealth = new System.Windows.Forms.CheckBox();
+            this.cbPagibig = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotalDeduction = new System.Windows.Forms.Label();
+            this.lblLoanDetails = new System.Windows.Forms.Label();
+            this.lblNetPay = new System.Windows.Forms.Label();
+            this.rbWithoutLoan = new System.Windows.Forms.RadioButton();
+            this.rbWithLoan = new System.Windows.Forms.RadioButton();
+            this.cbEmpRank = new System.Windows.Forms.ComboBox();
+            this.tbEmpName = new System.Windows.Forms.TextBox();
+            this.tbHrsWorked = new System.Windows.Forms.TextBox();
+            this.tbGrossSalary = new System.Windows.Forms.TextBox();
+            this.tbTtlDeductions = new System.Windows.Forms.TextBox();
+            this.tbNetPay = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEmpName
+            // 
+            this.lblEmpName.AutoSize = true;
+            this.lblEmpName.Location = new System.Drawing.Point(12, 59);
+            this.lblEmpName.Name = "lblEmpName";
+            this.lblEmpName.Size = new System.Drawing.Size(97, 15);
+            this.lblEmpName.TabIndex = 0;
+            this.lblEmpName.Text = "Employee Name:";
+            // 
+            // lblHrsWorked
+            // 
+            this.lblHrsWorked.AutoSize = true;
+            this.lblHrsWorked.Location = new System.Drawing.Point(12, 86);
+            this.lblHrsWorked.Name = "lblHrsWorked";
+            this.lblHrsWorked.Size = new System.Drawing.Size(122, 15);
+            this.lblHrsWorked.TabIndex = 1;
+            this.lblHrsWorked.Text = "No of. Hours Worked:";
+            // 
+            // lblEmpRank
+            // 
+            this.lblEmpRank.AutoSize = true;
+            this.lblEmpRank.Location = new System.Drawing.Point(12, 121);
+            this.lblEmpRank.Name = "lblEmpRank";
+            this.lblEmpRank.Size = new System.Drawing.Size(91, 15);
+            this.lblEmpRank.TabIndex = 2;
+            this.lblEmpRank.Text = "Employee Rank:";
+            // 
+            // lblGrossSalary
+            // 
+            this.lblGrossSalary.AutoSize = true;
+            this.lblGrossSalary.Location = new System.Drawing.Point(13, 152);
+            this.lblGrossSalary.Name = "lblGrossSalary";
+            this.lblGrossSalary.Size = new System.Drawing.Size(73, 15);
+            this.lblGrossSalary.TabIndex = 3;
+            this.lblGrossSalary.Text = "Gross Salary:";
+            this.lblGrossSalary.Click += new System.EventHandler(this.lblGrossSalary_Click);
+            // 
+            // lblDeductions
+            // 
+            this.lblDeductions.AutoSize = true;
+            this.lblDeductions.Location = new System.Drawing.Point(12, 184);
+            this.lblDeductions.Name = "lblDeductions";
+            this.lblDeductions.Size = new System.Drawing.Size(70, 15);
+            this.lblDeductions.TabIndex = 4;
+            this.lblDeductions.Text = "Deductions:";
+            // 
+            // cbGSIS
+            // 
+            this.cbGSIS.AutoSize = true;
+            this.cbGSIS.Location = new System.Drawing.Point(20, 212);
+            this.cbGSIS.Name = "cbGSIS";
+            this.cbGSIS.Size = new System.Drawing.Size(120, 19);
+            this.cbGSIS.TabIndex = 5;
+            this.cbGSIS.Text = "GSIS Contribution";
+            this.cbGSIS.UseVisualStyleBackColor = true;
+            // 
+            // cbWithholdingTax
+            // 
+            this.cbWithholdingTax.AutoSize = true;
+            this.cbWithholdingTax.Location = new System.Drawing.Point(20, 248);
+            this.cbWithholdingTax.Name = "cbWithholdingTax";
+            this.cbWithholdingTax.Size = new System.Drawing.Size(123, 19);
+            this.cbWithholdingTax.TabIndex = 6;
+            this.cbWithholdingTax.Text = "Witholding Tax      ";
+            this.cbWithholdingTax.UseVisualStyleBackColor = true;
+            // 
+            // cbPhilHealth
+            // 
+            this.cbPhilHealth.AutoSize = true;
+            this.cbPhilHealth.Location = new System.Drawing.Point(220, 212);
+            this.cbPhilHealth.Name = "cbPhilHealth";
+            this.cbPhilHealth.Size = new System.Drawing.Size(94, 19);
+            this.cbPhilHealth.TabIndex = 7;
+            this.cbPhilHealth.Text = "PHILHEALTH";
+            this.cbPhilHealth.UseVisualStyleBackColor = true;
+            // 
+            // cbPagibig
+            // 
+            this.cbPagibig.AutoSize = true;
+            this.cbPagibig.Location = new System.Drawing.Point(220, 248);
+            this.cbPagibig.Name = "cbPagibig";
+            this.cbPagibig.Size = new System.Drawing.Size(74, 19);
+            this.cbPagibig.TabIndex = 8;
+            this.cbPagibig.Text = "PAG-IBIG";
+            this.cbPagibig.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "P1000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "10%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "P200";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "P300";
+            // 
+            // lblTotalDeduction
+            // 
+            this.lblTotalDeduction.AutoSize = true;
+            this.lblTotalDeduction.Location = new System.Drawing.Point(12, 284);
+            this.lblTotalDeduction.Name = "lblTotalDeduction";
+            this.lblTotalDeduction.Size = new System.Drawing.Size(98, 15);
+            this.lblTotalDeduction.TabIndex = 13;
+            this.lblTotalDeduction.Text = "Total Deductions:";
+            // 
+            // lblLoanDetails
+            // 
+            this.lblLoanDetails.AutoSize = true;
+            this.lblLoanDetails.Location = new System.Drawing.Point(12, 314);
+            this.lblLoanDetails.Name = "lblLoanDetails";
+            this.lblLoanDetails.Size = new System.Drawing.Size(74, 15);
+            this.lblLoanDetails.TabIndex = 14;
+            this.lblLoanDetails.Text = "Loan Details:";
+            // 
+            // lblNetPay
+            // 
+            this.lblNetPay.AutoSize = true;
+            this.lblNetPay.Location = new System.Drawing.Point(12, 345);
+            this.lblNetPay.Name = "lblNetPay";
+            this.lblNetPay.Size = new System.Drawing.Size(51, 15);
+            this.lblNetPay.TabIndex = 15;
+            this.lblNetPay.Text = "Net Pay:";
+            // 
+            // rbWithoutLoan
+            // 
+            this.rbWithoutLoan.AutoSize = true;
+            this.rbWithoutLoan.Checked = true;
+            this.rbWithoutLoan.Location = new System.Drawing.Point(3, 4);
+            this.rbWithoutLoan.Name = "rbWithoutLoan";
+            this.rbWithoutLoan.Size = new System.Drawing.Size(97, 19);
+            this.rbWithoutLoan.TabIndex = 16;
+            this.rbWithoutLoan.TabStop = true;
+            this.rbWithoutLoan.Text = "Without Loan";
+            this.rbWithoutLoan.UseVisualStyleBackColor = true;
+            // 
+            // rbWithLoan
+            // 
+            this.rbWithLoan.AutoSize = true;
+            this.rbWithLoan.Location = new System.Drawing.Point(116, 4);
+            this.rbWithLoan.Name = "rbWithLoan";
+            this.rbWithLoan.Size = new System.Drawing.Size(79, 19);
+            this.rbWithLoan.TabIndex = 17;
+            this.rbWithLoan.TabStop = true;
+            this.rbWithLoan.Text = "With Loan";
+            this.rbWithLoan.UseVisualStyleBackColor = true;
+            // 
+            // cbEmpRank
+            // 
+            this.cbEmpRank.FormattingEnabled = true;
+            this.cbEmpRank.Items.AddRange(new object[] {
+            "Rank 1",
+            "Rank 2",
+            "Rank 3"});
+            this.cbEmpRank.Location = new System.Drawing.Point(141, 118);
+            this.cbEmpRank.Name = "cbEmpRank";
+            this.cbEmpRank.Size = new System.Drawing.Size(173, 23);
+            this.cbEmpRank.TabIndex = 18;
+            // 
+            // tbEmpName
+            // 
+            this.tbEmpName.Location = new System.Drawing.Point(141, 51);
+            this.tbEmpName.Name = "tbEmpName";
+            this.tbEmpName.Size = new System.Drawing.Size(173, 23);
+            this.tbEmpName.TabIndex = 19;
+            // 
+            // tbHrsWorked
+            // 
+            this.tbHrsWorked.Location = new System.Drawing.Point(141, 83);
+            this.tbHrsWorked.Name = "tbHrsWorked";
+            this.tbHrsWorked.Size = new System.Drawing.Size(173, 23);
+            this.tbHrsWorked.TabIndex = 20;
+            // 
+            // tbGrossSalary
+            // 
+            this.tbGrossSalary.Location = new System.Drawing.Point(141, 149);
+            this.tbGrossSalary.Name = "tbGrossSalary";
+            this.tbGrossSalary.ReadOnly = true;
+            this.tbGrossSalary.Size = new System.Drawing.Size(173, 23);
+            this.tbGrossSalary.TabIndex = 21;
+            // 
+            // tbTtlDeductions
+            // 
+            this.tbTtlDeductions.Location = new System.Drawing.Point(141, 281);
+            this.tbTtlDeductions.Name = "tbTtlDeductions";
+            this.tbTtlDeductions.ReadOnly = true;
+            this.tbTtlDeductions.Size = new System.Drawing.Size(173, 23);
+            this.tbTtlDeductions.TabIndex = 22;
+            // 
+            // tbNetPay
+            // 
+            this.tbNetPay.Location = new System.Drawing.Point(141, 342);
+            this.tbNetPay.Name = "tbNetPay";
+            this.tbNetPay.ReadOnly = true;
+            this.tbNetPay.Size = new System.Drawing.Size(173, 23);
+            this.tbNetPay.TabIndex = 23;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(35, 396);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(99, 23);
+            this.btnCompute.TabIndex = 24;
+            this.btnCompute.Text = "btn-&COMPUTE";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(157, 396);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 25;
+            this.btnClear.Text = "btn-CLEA&R";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(255, 395);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 26;
+            this.btnClose.Text = "btn-CLO&SE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rbWithoutLoan);
+            this.panel1.Controls.Add(this.rbWithLoan);
+            this.panel1.Location = new System.Drawing.Point(119, 310);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 26);
+            this.panel1.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.tbNetPay);
+            this.Controls.Add(this.tbTtlDeductions);
+            this.Controls.Add(this.tbGrossSalary);
+            this.Controls.Add(this.tbHrsWorked);
+            this.Controls.Add(this.tbEmpName);
+            this.Controls.Add(this.cbEmpRank);
+            this.Controls.Add(this.lblNetPay);
+            this.Controls.Add(this.lblLoanDetails);
+            this.Controls.Add(this.lblTotalDeduction);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbPagibig);
+            this.Controls.Add(this.cbPhilHealth);
+            this.Controls.Add(this.cbWithholdingTax);
+            this.Controls.Add(this.cbGSIS);
+            this.Controls.Add(this.lblDeductions);
+            this.Controls.Add(this.lblGrossSalary);
+            this.Controls.Add(this.lblEmpRank);
+            this.Controls.Add(this.lblHrsWorked);
+            this.Controls.Add(this.lblEmpName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmpName;
+        private System.Windows.Forms.Label lblHrsWorked;
+        private System.Windows.Forms.Label lblEmpRank;
+        private System.Windows.Forms.Label lblGrossSalary;
+        private System.Windows.Forms.Label lblDeductions;
+        private System.Windows.Forms.CheckBox cbGSIS;
+        private System.Windows.Forms.CheckBox cbWithholdingTax;
+        private System.Windows.Forms.CheckBox cbPhilHealth;
+        private System.Windows.Forms.CheckBox cbPagibig;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotalDeduction;
+        private System.Windows.Forms.Label lblLoanDetails;
+        private System.Windows.Forms.Label lblNetPay;
+        private System.Windows.Forms.RadioButton rbWithoutLoan;
+        private System.Windows.Forms.RadioButton rbWithLoan;
+        private System.Windows.Forms.ComboBox cbEmpRank;
+        private System.Windows.Forms.TextBox tbEmpName;
+        private System.Windows.Forms.TextBox tbHrsWorked;
+        private System.Windows.Forms.TextBox tbGrossSalary;
+        private System.Windows.Forms.TextBox tbTtlDeductions;
+        private System.Windows.Forms.TextBox tbNetPay;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Employees Salary/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employees_Salary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cbEmpRank.SelectedIndex = -1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGrossSalary_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
